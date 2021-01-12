4b61d833c18381ccd3fd6a33c9fc5b12cea487be
waitting <1592021024@qq.com>
2021-01-12 21:06:14 +0800

    db2

--- sql_app/database.py
+++ sql_app/database.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql://root:123456@localhost:3306/videolist?charset=utf8"
+SQLALCHEMY_DATABASE_URL = "mysql://yyj:123456qwerty@localhost:3306/videolist?charset=utf8"
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
 )
